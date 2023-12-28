ba3a765a55675d5705d660d02a3185a22f09b0b7
lucasfelix1803 <lucasfelix210917@gmail.com>
2023-12-28 10:00:57 -0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,12 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'artistas', views.ArtistaViewSet)
-router.register(r'albuns', views.AlbumViewSet)
-router.register(r'musicas', views.MusicaViewSet)
+router.register(r'users', views.UserViewSet)
+users_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
+users_router.register(r'todos', views.TodoViewSet)
+router.register(r'todos', views.TodoViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'posts', views.PostViewSet)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
