2c8a30818eedbb1178ec2952c1b9d91095a3a437
lucasfelix1803 <lucasfelix210917@gmail.com>
2023-12-28 08:05:45 -0300

    models

--- api/models.py
+++ api/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+class post(models.Model):
+    userid = models.ForeignKey(max_length=100)
+    id = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=100)
 
-class Artista(models.Model):
-    nome = models.CharField(max_length=100)
-    local = models.CharField(max_length=100)
-    ano_criacao = models.IntegerField()
 
-
-def __str__(self):
-    return self.nome
-
+class todos(models.Model):
+    userid = models.ForeignKey(max_length=100)
+    id = models.CharField(max_length=100)    
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(max_length=100)
 
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -25,7 +27,3 @@ def __str__(self):
     return self.name
 
 
-class Musica(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    nome = models.CharField(max_length=100)
-    segundos = models.IntegerField()
