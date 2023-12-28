a1ef2209ce1090288f565cc456c2242122debcc0
agstbelle <isabellepontes91@gmail.com>
2023-12-28 09:47:49 -0300

    serializers e views

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'artistas', views.ArtistaViewSet)
-router.register(r'albuns', views.AlbumViewSet)
-router.register(r'musicas', views.MusicaViewSet)
+router.register(r'User', views.UserViewSet)
+router.register(r'Todos', views.TodosViewSet)
+router.register(r'Post', views.PostViewSet)
+router.register(r'Comment', views.CommentViewSet)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
