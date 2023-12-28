df32efe94efd46c0ae2674d7a8077bb74dd0b4b2
lucasfelix1803 <lucasfelix210917@gmail.com>
2023-12-28 10:03:06 -0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_nested import routers
+from rest_framework import routers
 
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'artistas', views.ArtistaViewSet)
-router.register(r'albuns', views.AlbumViewSet)
-router.register(r'musicas', views.MusicaViewSet)
+router.register(r'users', views.UserViewSet)
+users_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
+users_router.register(r'todos', views.TodoViewSet)
+router.register(r'todos', views.TodoViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'posts', views.PostViewSet)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
