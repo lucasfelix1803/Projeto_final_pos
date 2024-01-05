eab2a9cb6a275bf9798ff0e02a32ae37b477f795
agstbelle <isabellepontes91@gmail.com>
2024-01-05 08:33:55 -0300

    correção de erros

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 users_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
-users_router.register(r'todos', views.TodoViewSet)
-router.register(r'todos', views.TodoViewSet)
+users_router.register(r'todos', views.TodosViewSet)
+router.register(r'todos', views.TodosViewSet)
 router.register(r'comments', views.CommentViewSet)
 router.register(r'posts', views.PostViewSet)
 
